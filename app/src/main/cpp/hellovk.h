3f208b50f67072f56e96142f6f9bead4bfda3ccd
Hakim Hauston <hakimh@google.com>
2024-03-07 13:35:50 +0800

    [codelab] step 6: create shader and pipeline - needs descriptor set layout

--- app/src/main/cpp/hellovk.h
+++ app/src/main/cpp/hellovk.h
@@ -192,7 +192,7 @@ class HelloVK {
   void createSwapChain();
   void createImageViews();
   void createRenderPass();
-  // void createDescriptorSetLayout();
+  void createDescriptorSetLayout();
   void createGraphicsPipeline();
   void createFramebuffers();
   // void createCommandPool();
@@ -287,11 +287,11 @@ void HelloVK::initVulkan() {
   createSwapChain();
   createImageViews();
   createRenderPass();
-//   createDescriptorSetLayout();
+  createDescriptorSetLayout();
 //   createUniformBuffers();
 //   createDescriptorPool();
 //   createDescriptorSets();
-//   createGraphicsPipeline();
+  createGraphicsPipeline();
   createFramebuffers();
 //   createCommandPool();
 //   createCommandBuffer();
@@ -365,22 +365,22 @@ void HelloVK::initVulkan() {
 //   }
 // }
 
-// void HelloVK::createDescriptorSetLayout() {
-//   VkDescriptorSetLayoutBinding uboLayoutBinding{};
-//   uboLayoutBinding.binding = 0;
-//   uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-//   uboLayoutBinding.descriptorCount = 1;
-//   uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
-//   uboLayoutBinding.pImmutableSamplers = nullptr;
+void HelloVK::createDescriptorSetLayout() {
+  VkDescriptorSetLayoutBinding uboLayoutBinding{};
+  uboLayoutBinding.binding = 0;
+  uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+  uboLayoutBinding.descriptorCount = 1;
+  uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+  uboLayoutBinding.pImmutableSamplers = nullptr;
 
-//   VkDescriptorSetLayoutCreateInfo layoutInfo{};
-//   layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-//   layoutInfo.bindingCount = 1;
-//   layoutInfo.pBindings = &uboLayoutBinding;
+  VkDescriptorSetLayoutCreateInfo layoutInfo{};
+  layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+  layoutInfo.bindingCount = 1;
+  layoutInfo.pBindings = &uboLayoutBinding;
 
-//   VK_CHECK(vkCreateDescriptorSetLayout(device, &layoutInfo, nullptr,
-//                                        &descriptorSetLayout));
-// }
+  VK_CHECK(vkCreateDescriptorSetLayout(device, &layoutInfo, nullptr,
+                                       &descriptorSetLayout));
+}
 
 void HelloVK::reset(ANativeWindow *newWindow, AAssetManager *newManager) {
   window.reset(newWindow);
