32a70c2af21ecd11d20c9621caecdb7ef5bbfd4a
Hakim Hauston <hakimh@google.com>
2024-03-21 12:50:29 +0800

    [codelab] step 0: empty app

--- app/src/main/cpp/hellovk.h
+++ app/src/main/cpp/hellovk.h
@@ -79,49 +79,49 @@ struct ANativeWindowDeleter {
 
 class HelloVK {
  public:
-  void initVulkan();
-  void render();
-  void cleanup();
-  void cleanupSwapChain();
-  void reset(ANativeWindow *newWindow, AAssetManager *newManager);
-  bool initialized = false;
+  // void initVulkan();
+  // void render();
+  // void cleanup();
+  // void cleanupSwapChain();
+  // void reset(ANativeWindow *newWindow, AAssetManager *newManager);
+  // bool initialized = false;
 
  private:
-  void createDevice();
-  void createInstance();
-  void createSurface();
-  void setupDebugMessenger();
-  void pickPhysicalDevice();
-  void createLogicalDeviceAndQueue();
-  void createSwapChain();
-  void createImageViews();
-  void createRenderPass();
-  void createDescriptorSetLayout();
-  void createGraphicsPipeline();
-  void createFrameBuffers();
-  void createCommandPool();
-  void createCommandBuffer();
-  void createSyncObjects();
-  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice candidateDevice);
-  bool checkDeviceExtensionSupport(VkPhysicalDevice candidateDevice);
-  bool isDeviceSuitable(VkPhysicalDevice candidateDevice);
-  bool checkValidationLayerSupport();
-  std::vector<const char *> getRequiredExtensions();
-  SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice candidateDevice);
-  VkShaderModule createShaderModule(const std::vector<uint8_t> &code);
-  void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
-  void recreateSwapChain();
-  void onOrientationChange();
-  uint32_t findMemoryType(uint32_t typeFilter,
-                          VkMemoryPropertyFlags properties);
-  void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage,
-                    VkMemoryPropertyFlags properties, VkBuffer &buffer,
-                    VkDeviceMemory &bufferMemory);
-  void createUniformBuffers();
-  void updateUniformBuffer(uint32_t currentImage);
-  void createDescriptorPool();
-  void createDescriptorSets();
-  void establishDisplaySizeIdentity();
+  // void createDevice();
+  // void createInstance();
+  // void createSurface();
+  // void setupDebugMessenger();
+  // void pickPhysicalDevice();
+  // void createLogicalDeviceAndQueue();
+  // void createSwapChain();
+  // void createImageViews();
+  // void createRenderPass();
+  // void createDescriptorSetLayout();
+  // void createGraphicsPipeline();
+  // void createFrameBuffers();
+  // void createCommandPool();
+  // void createCommandBuffer();
+  // void createSyncObjects();
+  // QueueFamilyIndices findQueueFamilies(VkPhysicalDevice candidateDevice);
+  // bool checkDeviceExtensionSupport(VkPhysicalDevice candidateDevice);
+  // bool isDeviceSuitable(VkPhysicalDevice candidateDevice);
+  // bool checkValidationLayerSupport();
+  // std::vector<const char *> getRequiredExtensions();
+  // SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice candidateDevice);
+  // VkShaderModule createShaderModule(const std::vector<uint8_t> &code);
+  // void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+  // void recreateSwapChain();
+  // void onOrientationChange();
+  // uint32_t findMemoryType(uint32_t typeFilter,
+  //                         VkMemoryPropertyFlags properties);
+  // void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage,
+  //                   VkMemoryPropertyFlags properties, VkBuffer &buffer,
+  //                   VkDeviceMemory &bufferMemory);
+  // void createUniformBuffers();
+  // void updateUniformBuffer(uint32_t currentImage);
+  // void createDescriptorPool();
+  // void createDescriptorSets();
+  // void establishDisplaySizeIdentity();
 
   /*
    * In order to enable validation layer toggle this to true and
@@ -131,53 +131,53 @@ class HelloVK {
    *
    * The validation layers are not shipped with the APK as they are sizeable.
    */
-  bool enableValidationLayers = false;
+  // bool enableValidationLayers = false;
 
-  const std::vector<const char *> validationLayers = {
-      "VK_LAYER_KHRONOS_validation"};
-  const std::vector<const char *> deviceExtensions = {
-      VK_KHR_SWAPCHAIN_EXTENSION_NAME};
-  std::unique_ptr<ANativeWindow, ANativeWindowDeleter> window;
-  AAssetManager *assetManager;
+  // const std::vector<const char *> validationLayers = {
+  //     "VK_LAYER_KHRONOS_validation"};
+  // const std::vector<const char *> deviceExtensions = {
+  //     VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+  // std::unique_ptr<ANativeWindow, ANativeWindowDeleter> window;
+  // AAssetManager *assetManager;
 
-  VkInstance instance;
-  VkDebugUtilsMessengerEXT debugMessenger;
+  // VkInstance instance;
+  // VkDebugUtilsMessengerEXT debugMessenger;
 
-  VkSurfaceKHR surface;
+  // VkSurfaceKHR surface;
 
-  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-  VkDevice device;
+  // VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+  // VkDevice device;
 
-  VkSwapchainKHR swapChain;
-  std::vector<VkImage> swapChainImages;
-  VkFormat swapChainImageFormat;
-  VkExtent2D swapChainExtent;
-  VkExtent2D displaySizeIdentity;
-  std::vector<VkImageView> swapChainImageViews;
-  std::vector<VkFramebuffer> swapChainFrameBuffers;
-  VkCommandPool commandPool;
-  std::vector<VkCommandBuffer> commandBuffers;
+  // VkSwapchainKHR swapChain;
+  // std::vector<VkImage> swapChainImages;
+  // VkFormat swapChainImageFormat;
+  // VkExtent2D swapChainExtent;
+  // VkExtent2D displaySizeIdentity;
+  // std::vector<VkImageView> swapChainImageViews;
+  // std::vector<VkFramebuffer> swapChainFrameBuffers;
+  // VkCommandPool commandPool;
+  // std::vector<VkCommandBuffer> commandBuffers;
 
-  VkQueue graphicsQueue;
-  VkQueue presentQueue;
+  // VkQueue graphicsQueue;
+  // VkQueue presentQueue;
 
-  VkRenderPass renderPass;
-  VkDescriptorSetLayout descriptorSetLayout;
-  VkPipelineLayout pipelineLayout;
-  VkPipeline graphicsPipeline;
+  // VkRenderPass renderPass;
+  // VkDescriptorSetLayout descriptorSetLayout;
+  // VkPipelineLayout pipelineLayout;
+  // VkPipeline graphicsPipeline;
 
-  std::vector<VkBuffer> uniformBuffers;
-  std::vector<VkDeviceMemory> uniformBuffersMemory;
+  // std::vector<VkBuffer> uniformBuffers;
+  // std::vector<VkDeviceMemory> uniformBuffersMemory;
 
-  std::vector<VkSemaphore> imageAvailableSemaphores;
-  std::vector<VkSemaphore> renderFinishedSemaphores;
-  std::vector<VkFence> inFlightFences;
-  VkDescriptorPool descriptorPool;
-  std::vector<VkDescriptorSet> descriptorSets;
+  // std::vector<VkSemaphore> imageAvailableSemaphores;
+  // std::vector<VkSemaphore> renderFinishedSemaphores;
+  // std::vector<VkFence> inFlightFences;
+  // VkDescriptorPool descriptorPool;
+  // std::vector<VkDescriptorSet> descriptorSets;
 
-  uint32_t currentFrame = 0;
-  bool orientationChanged = false;
-  VkSurfaceTransformFlagBitsKHR preTransformFlag;
+  // uint32_t currentFrame = 0;
+  // bool orientationChanged = false;
+  // VkSurfaceTransformFlagBitsKHR preTransformFlag;
 };
 
 }  // namespace vkt
